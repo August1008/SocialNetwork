d6809d6edff1de91be7ef9b7c9fc36ae0d9d23f7
August1008 <ngbinhan1512@gmail.com>
2023-02-17 19:24:39 +0700

    Add settings to connect mongodb.

---
+++ SocialNetwork/Models/ISocialNetworkDatabaseSetting.cs
@@ -0,0 +1,13 @@
+﻿namespace SocialNetwork.Models
+{
+    public interface ISocialNetworkDatabaseSetting
+    {
+        public string RegistrationCollection { get; set; }
+        public string ArticleCollection { get; set; }
+        public string NewCollection { get; set; }
+        public string EventCollection { get; set; }
+        public string DatabaseName { get; set; }
+        public string ConnectionString { get; set; }
+
+    }
+}

1e87dbc4a41bd658a3d10563e03923048f8612b5
August1008 <ngbinhan1512@gmail.com>
2023-02-17 20:47:30 +0700

    Implement 70% registration service.

--- SocialNetwork/Models/SocialNetworkDatabaseSetting.cs
+++ SocialNetwork/Models/SocialNetworkDatabaseSetting.cs
@@ -6,7 +6,7 @@
         public string ArticleCollection { get; set; } = string.Empty;
         public string NewCollection { get; set; } = string.Empty;
         public string EventCollection { get; set; } = string.Empty;
-        public string DatabaseName { get; set; } = string.Empty;
-        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = String.Empty;
+        public string ConnectionString { get; set; } = String.Empty;
     }
 }
---
+++ SocialNetwork/Services/RegistrationService.cs
@@ -0,0 +1,52 @@
+﻿using SocialNetwork.Models;
+using MongoDB.Driver;
+namespace SocialNetwork.Services
+{
+    public class RegistrationService : IRegistrationService
+    {
+        private readonly IMongoCollection<Registration> _registrations;
+        public RegistrationService(ISocialNetworkDatabaseSetting setting, IMongoClient mongoClient) {
+            var database = mongoClient.GetDatabase(setting.DatabaseName);
+            _registrations = database.GetCollection<Registration>(setting.RegistrationCollection);
+        }
+        public bool Create(Registration registration)
+        {
+            try
+            {
+                _registrations.InsertOne(registration);
+            } catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public IEnumerable<Registration> Get()
+        {
+            return _registrations.Find(registration => true).ToList();
+        }
+
+        public Registration GetById(string id)
+        {
+            return _registrations.Find(registration => registration.Id == id).FirstOrDefault();
+        }
+
+        public bool Remove(Registration registration)
+        {
+            try
+            {
+                _registrations.DeleteOne(regis => regis.Id == registration.Id );
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool Update(Registration registration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
